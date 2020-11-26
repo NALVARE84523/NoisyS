8b0507e1872cf78e48d88ab1da9852413f65f622
Nelson Alvarez <nalvare84523@universidadean.edu.co>
2020-11-26 15:20:16 -0500

    Back, error en actualizar usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
+var jwt = require('../services/jwt');
 
 function pruebas(req, res){
     res.status(200).send({
@@ -18,18 +19,94 @@ function saveUser(req, res){
     user.name = params.name;
     user.surname = params.surname;
     user.email = params.email;
-    user.role = 'ROLE_USER';
+    user.role = 'ROLE_ADMIN';
     user.image = 'null';
 
     if(params.password){
-        // Encriptar contraseña y guardar datos
+        // Encriptar contraseña
+        bcrypt.hash(params.password, null, null, function(err, hash){
+            user.password = hash;
+
+            if(user.name != null && user.surname != null && user.email != null){
+                // Guardar el usuario
+                user.save((err, userStored) => {
+                    if (err) {
+                        res.status(500).send({message: 'Error al guardar el usuario'});
+                    } else {
+                        if(!userStored){
+                            res.status(404).send({message: 'no se ha registrado el usuario'});
+                        }else{
+                            res.status(200).send({user: userStored});
+                        }
+                    }
+                });
+
+            }else{
+                res.status(200).send({message: 'Introduce todos los campos'});
+            }
+        });
     }else{
-    res.status(500).send({message: 'Introduce la contraseña'});
+    res.status(200).send({message: 'Introduce la contraseña'});
     }
 
 }
 
+function loginUser(req, res){
+    var params = req.body;
+
+    var email = params.email;
+    var password = params.password;
+
+    User.findOne({ email: email.toLowerCase()}, (err, user) => {
+        if(err){
+            res.status(500).send({ message: 'Error en la peticion' });
+        }else{
+            if(!user){
+                res.status(404).send({ message: 'El usuario no existe' });
+            }else{
+
+                // Comprobar la contrasena
+                bcrypt.compare(password, user.password, function(err, check){
+                    if(check){
+                        // Devolver los datos del usuario logueado
+                        if(params.gethash){
+                            // Devolver un token de jwt
+                            res.status(200).send({
+                                token: jwt.createToken(user)
+                            })
+                        }else{
+                            res.status(200).send({user});
+                        }
+                    }else{
+                        res.status(404).send({ message: 'El usuario no ha podido loguearse' });
+                    }
+                })
+            }
+        }
+    })
+}
+
+function updateUser(req, res){
+    var userId = req.params.id;
+    var update = req.body;
+
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) =>{
+        if(err){
+            res.status(500).send({ message: 'Error al actualizar el usuario' });
+        }else{
+            if(!userUpdated){
+                res.status(404).send({ message: 'No se ha podido actualizar el usuario' });
+            }else{
+                res.status(200).send({user: userUpdated})
+            }
+        }
+    })
+}
+
+
 module.exports = {
     pruebas,
-    saveUser
+    saveUser,
+    loginUser,
+    updateUser
 };
